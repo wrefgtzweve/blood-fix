b5ec7d55df5e16e524c0f8c47a87e831ec0be498
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-08-09 03:19:50 +0200

    first commit

---
+++ lua/autorun/server/sv_blood_hit.lua
@@ -0,0 +1,22 @@
+local function playEffects( ent, data )
+    if not ent:IsPlayer() or not data:IsBulletDamage() then return end
+    if ent:GetBloodColor() == 0 then
+        ent:SetBloodColor( -1 )
+    end
+
+    if ent:GetBloodColor() ~= -1 then return end
+
+    local hitpos = data:GetDamagePosition()
+    local inflictorEyepos = data:GetAttacker():EyePos()
+    local effectdata = EffectData()
+    effectdata:SetOrigin( hitpos )
+    util.Effect( "BloodImpact", effectdata )
+
+    local tempBloodPos = ( hitpos + ( ( inflictorEyepos - hitpos ):GetNormalized() * math.random( -25, -200 ) ) ) + Vector( math.random( -15, 15 ), math.random( -15, 15 ), 0 )
+    local bloodPos = tempBloodPos - Vector( 0, 0, 75 )
+
+    util.Decal( "Blood", hitpos, tempBloodPos, ent )
+    util.Decal( "Blood", tempBloodPos, bloodPos, ent )
+end
+
+hook.Add( "PostEntityTakeDamage", "CFC_ResponsiveHits_PostEntityTakeDamage", playEffects )
